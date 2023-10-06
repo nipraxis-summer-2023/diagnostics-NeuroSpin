36d67545bc540f7113f637de91a46fba6830502f
Ulysse Boureau <ulysse.boureau@outlook.fr>
2023-10-06 18:26:09 +0200

    add z-score threshold and the use of spm metrics

--- findoutlie/detectors.py
+++ findoutlie/detectors.py
@@ -59,3 +59,39 @@ def iqr_detector(measures, iqr_proportion=1.5):
     down_thresh = q1 - iqr * iqr_proportion
     return np.logical_or(measures > up_thresh, measures < down_thresh)
 
+
+def zscore_detector(measures, threshold=2.5):
+    """ Detect outliers in `measures` using Z-Score thresholding.
+
+    Returns a boolean vector of same length as `measures`, where True means the
+    corresponding value in `measures` is an outlier.
+
+    Call m = mean(`measures`) and std = standard_deviation(`measures`)
+
+    For each value x_i in `measure` we compute z_i = (x_i - m)/std
+
+    An outlier is any value x_i in `measures` so that:
+
+    | z_i | > threshold
+
+
+    Parameters
+    ----------
+    measures : 1D array
+        Values for which we will detect outliers
+    threshold : float, optional
+        Scalar Z-score threshold for outlier detection (see
+        above).  Default is 3.
+
+    Returns
+    -------
+    outlier_tf : 1D boolean array
+        A boolean vector of same length as `measures`, where True means the
+        corresponding value in `measures` is an outlier.
+    """
+
+    m = np.mean(measures)
+    std = np.std(measures)
+    z_measures = (measures - m)/std
+
+    return np.abs(z_measures) > threshold
--- findoutlie/outfind.py
+++ findoutlie/outfind.py
@@ -3,21 +3,38 @@
 import sys
 from pathlib import Path
 import nibabel as nib
+import numpy as np
+import pandas as pd
 
 PACKAGE_DIR = Path(__file__).parent / '..'
 sys.path.append(str(PACKAGE_DIR))
 
 from findoutlie.metrics import dvars
-from findoutlie.detectors import iqr_detector
+from findoutlie.detectors import iqr_detector, zscore_detector
+from findoutlie.spm_funcs import get_spm_globals
 
 
 def detect_outliers(fname):
     img = nib.load(fname)
 
-    # find iqr outliers from dvars
+    # compute metrics
     dvars_values = dvars(img)
-    outliers = iqr_detector(dvars_values)
+    spm_values = get_spm_globals(fname)
 
+    # compute outlier flags from detectors
+    outliers_dvars_iqr = np.concatenate([[False],iqr_detector(dvars_values, iqr_proportion=3)])  # We add false for the first picture because dvars metrics is of size n-1; the first image is not analysed
+    outliers_dvars_zscore = np.concatenate([[False],zscore_detector(dvars_values, 3)])
+
+    outliers_spm_iqr = iqr_detector(spm_values, iqr_proportion=3)
+    outliers_spm_zscore = zscore_detector(spm_values, 3)
+
+    outliers = np.full(img.shape[-1], False)
+    for i in range(img.shape[-1]):
+        # image i is an outlier if for both metrics at least one detector flag it
+        outliers[i] = outliers_dvars_iqr[i] + outliers_spm_iqr[i] + outliers_dvars_zscore[i] + outliers_spm_zscore[i]
+
+    #outliers_all = pd.DataFrame(np.array([outliers_dvars_iqr, outliers_dvars_zscore, outliers_spm_iqr, outliers_spm_zscore]).T, columns = ['dvars_iqr', 'dvars_zscore', 'spm_iqr', 'spm_zscore'], index = range(1, img.shape[-1] + 1))
+    #outliers_all = outliers_all[outliers_all.any(axis=1)]
     return outliers
 
 
